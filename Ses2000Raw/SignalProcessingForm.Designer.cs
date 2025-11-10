a05382a60ce0929922589e48189d423beabac837
Fujii Tomonori <t-fujii@windy-net.co.jp>
2025-11-10 17:50:14 +0900

    信号処理用ダイアログ追加

---
+++ Ses2000Raw/SignalProcessingForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Ses2000Raw
+{
+    partial class SignalProcessingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cmbDemodulate = new ComboBox();
+            groupBox1 = new GroupBox();
+            lblLpfMax = new Label();
+            lblLpfMin = new Label();
+            trackBarLpf = new TrackBar();
+            lblLpf = new Label();
+            label9 = new Label();
+            lblHpfMax = new Label();
+            lblHpfMin = new Label();
+            trackBarHpf = new TrackBar();
+            lblHpf = new Label();
+            label2 = new Label();
+            chkBpf = new CheckBox();
+            btnOk = new Button();
+            btnCancel = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarLpf).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarHpf).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Demodulate";
+            // 
+            // cmbDemodulate
+            // 
+            cmbDemodulate.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDemodulate.FormattingEnabled = true;
+            cmbDemodulate.Items.AddRange(new object[] { "None (Full Wave)", "Envelope" });
+            cmbDemodulate.Location = new Point(118, 20);
+            cmbDemodulate.Name = "cmbDemodulate";
+            cmbDemodulate.Size = new Size(201, 23);
+            cmbDemodulate.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(lblLpfMax);
+            groupBox1.Controls.Add(lblLpfMin);
+            groupBox1.Controls.Add(trackBarLpf);
+            groupBox1.Controls.Add(lblLpf);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(lblHpfMax);
+            groupBox1.Controls.Add(lblHpfMin);
+            groupBox1.Controls.Add(trackBarHpf);
+            groupBox1.Controls.Add(lblHpf);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(23, 58);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(355, 141);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            // 
+            // lblLpfMax
+            // 
+            lblLpfMax.Location = new Point(306, 103);
+            lblLpfMax.Name = "lblLpfMax";
+            lblLpfMax.Size = new Size(28, 23);
+            lblLpfMax.TabIndex = 10;
+            lblLpfMax.Text = "48";
+            lblLpfMax.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblLpfMin
+            // 
+            lblLpfMin.Location = new Point(95, 103);
+            lblLpfMin.Name = "lblLpfMin";
+            lblLpfMin.Size = new Size(28, 23);
+            lblLpfMin.TabIndex = 9;
+            lblLpfMin.Text = "0";
+            lblLpfMin.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // trackBarLpf
+            // 
+            trackBarLpf.Location = new Point(95, 81);
+            trackBarLpf.Name = "trackBarLpf";
+            trackBarLpf.Size = new Size(235, 45);
+            trackBarLpf.TabIndex = 8;
+            trackBarLpf.Tag = "Lpf";
+            trackBarLpf.TickStyle = TickStyle.None;
+            trackBarLpf.Scroll += trackBarBpf_Scroll;
+            // 
+            // lblLpf
+            // 
+            lblLpf.BorderStyle = BorderStyle.FixedSingle;
+            lblLpf.Location = new Point(49, 81);
+            lblLpf.Name = "lblLpf";
+            lblLpf.Size = new Size(40, 23);
+            lblLpf.TabIndex = 7;
+            lblLpf.Text = "0";
+            lblLpf.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(14, 85);
+            label9.Name = "label9";
+            label9.Size = new Size(26, 15);
+            label9.TabIndex = 6;
+            label9.Text = "LPF";
+            // 
+            // lblHpfMax
+            // 
+            lblHpfMax.Location = new Point(306, 52);
+            lblHpfMax.Name = "lblHpfMax";
+            lblHpfMax.Size = new Size(28, 23);
+            lblHpfMax.TabIndex = 5;
+            lblHpfMax.Text = "48";
+            lblHpfMax.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblHpfMin
+            // 
+            lblHpfMin.Location = new Point(95, 52);
+            lblHpfMin.Name = "lblHpfMin";
+            lblHpfMin.Size = new Size(28, 23);
+            lblHpfMin.TabIndex = 4;
+            lblHpfMin.Text = "0";
+            lblHpfMin.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // trackBarHpf
+            // 
+            trackBarHpf.Location = new Point(95, 30);
+            trackBarHpf.Name = "trackBarHpf";
+            trackBarHpf.Size = new Size(235, 45);
+            trackBarHpf.TabIndex = 3;
+            trackBarHpf.Tag = "Hpf";
+            trackBarHpf.TickStyle = TickStyle.None;
+            trackBarHpf.Scroll += trackBarBpf_Scroll;
+            // 
+            // lblHpf
+            // 
+            lblHpf.BorderStyle = BorderStyle.FixedSingle;
+            lblHpf.Location = new Point(49, 30);
+            lblHpf.Name = "lblHpf";
+            lblHpf.Size = new Size(40, 23);
+            lblHpf.TabIndex = 2;
+            lblHpf.Text = "0";
+            lblHpf.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 1;
+            label2.Text = "HPF";
+            // 
+            // chkBpf
+            // 
+            chkBpf.AutoSize = true;
+            chkBpf.Location = new Point(37, 58);
+            chkBpf.Name = "chkBpf";
+            chkBpf.Size = new Size(109, 19);
+            chkBpf.TabIndex = 0;
+            chkBpf.Text = "Band-pass-filter";
+            chkBpf.UseVisualStyleBackColor = true;
+            chkBpf.CheckedChanged += chkBpf_CheckedChanged;
+            // 
+            // btnOk
+            // 
+            btnOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnOk.Location = new Point(235, 220);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(75, 23);
+            btnOk.TabIndex = 3;
+            btnOk.Text = "OK";
+            btnOk.UseVisualStyleBackColor = true;
+            btnOk.Click += btnOk_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(316, 220);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 4;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // SignalProcessingForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(403, 255);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOk);
+            Controls.Add(chkBpf);
+            Controls.Add(groupBox1);
+            Controls.Add(cmbDemodulate);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "SignalProcessingForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Signal Processing";
+            Load += SignalProcessingForm_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBarLpf).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarHpf).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cmbDemodulate;
+        private GroupBox groupBox1;
+        private Label lblHpfMin;
+        private TrackBar trackBarHpf;
+        private Label lblHpf;
+        private Label label2;
+        private CheckBox chkBpf;
+        private Label lblLpfMax;
+        private Label lblLpfMin;
+        private TrackBar trackBarLpf;
+        private Label lblLpf;
+        private Label label9;
+        private Label lblHpfMax;
+        private Button btnOk;
+        private Button btnCancel;
+    }
+}
---
+++ Ses2000Raw/SignalProcessingForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ses2000Raw
+{
+    public partial class SignalProcessingForm : Form
+    {
+        AnalysisForm m_frmParent = null;
+
+        public SignalProcessingForm(AnalysisForm parent, double samplingFreqHz)
+        {
+            m_frmParent = parent;
+
+            InitializeComponent();
+
+            int iMaxFreq = (int)((samplingFreqHz / 2) / 1000);
+
+            this.trackBarHpf.Maximum = this.trackBarLpf.Maximum = iMaxFreq;
+            this.lblHpfMax.Text = this.lblLpfMax.Text = iMaxFreq.ToString();
+        }
+        /// <summary>
+        /// Form Load Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SignalProcessingForm_Load(object sender, EventArgs e)
+        {
+            this.cmbDemodulate.SelectedIndex = (int)m_frmParent.DemodulateMode;
+            this.chkBpf.Checked = m_frmParent.ApplyBpf;
+            this.chkBpf_CheckedChanged(this.chkBpf, EventArgs.Empty);
+            this.trackBarHpf.Value = m_frmParent.Hpf_kHz;
+            this.trackBarLpf.Value = m_frmParent.Lpf_kHz;
+            this.lblHpf.Text = this.trackBarHpf.Value.ToString();
+            this.lblLpf.Text = this.trackBarLpf.Value.ToString();
+        }
+        /// <summary>
+        /// chkBpf Checked Changed Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void chkBpf_CheckedChanged(object sender, EventArgs e)
+        {
+            this.groupBox1.Enabled = this.chkBpf.Checked;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBarBpf_Scroll(object sender, EventArgs e)
+        {
+            if (((TrackBar)sender).Tag == null) return;
+
+            switch (((TrackBar)sender).Tag.ToString())
+            {
+                case "Hpf":
+                    this.lblHpf.Text = trackBarHpf.Value.ToString();
+                    break;
+                case "Lpf":
+                    this.lblLpf.Text = trackBarLpf.Value.ToString();
+                    break;
+            }
+
+        }
+        /// <summary>
+        /// 「OK」ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool bEnvelope = (this.cmbDemodulate.SelectedIndex == (int)AnalysisForm.DemodulationMode.Envelope) ? true : false;
+                bool bBpf = this.chkBpf.Checked;
+
+                if (!bEnvelope && !bBpf)
+                {
+                    m_frmParent.DemodulateMode = (AnalysisForm.DemodulationMode)this.cmbDemodulate.SelectedIndex;
+                    m_frmParent.ApplyBpf = bBpf;
+                    m_frmParent.Hpf_kHz = this.trackBarHpf.Value;
+                    m_frmParent.Lpf_kHz = this.trackBarLpf.Value;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                    return;
+                }
+
+                // 入力取得（単位は一貫して Hz/kHz）
+                int fsHz = (int)m_frmParent.BlockHeaderList[0].SampleFrequencyForLf; // Hz
+                int hpf_kHz = this.trackBarHpf.Value;
+                int lpf_kHz = this.trackBarLpf.Value;
+
+                // BPFの妥当性チェック
+                if (bBpf)
+                {
+                    if (hpf_kHz < 0 || lpf_kHz <= 0 || hpf_kHz >= lpf_kHz)
+                        throw new ArgumentException("Band-pass設定が不正です（HPF < LPF を満たす必要があります）。");
+                    if (lpf_kHz * 1000.0 > fsHz / 2.0)
+                        throw new ArgumentException("LPFがナイキスト周波数を超えています。LPF を下げてください。");
+                }
+
+                // 進捗UI
+                using var frmProgress = new ProgressForm();
+                frmProgress.SetDiscription("Processing...");
+                frmProgress.SetProgressValue(0);
+                frmProgress.SetMaxValue(m_frmParent.DataBlockList.Count);
+                frmProgress.Show(this);
+
+                // IProgress は UI スレッドに自動マーシャルされる
+                var progress = new Progress<int>(v => frmProgress.SetProgressValue(v));
+
+                // 非同期実行
+                var ret = await Task.Run(() =>
+                    DataClass.SignalProcessParallel(
+                        /*context:*/ null,         // ← 不要
+                        progress,
+                        m_frmParent.DataBlockList,
+                        fsHz,
+                        bBpf,
+                        hpf_kHz,
+                        lpf_kHz,
+                        bEnvelope
+                    )
+                ).ConfigureAwait(true); // WinFormsなのでUIに戻す
+
+                // 結果処理
+                if (ret.RetCode == ResultCode.Ok)
+                {
+                    var proc = ret.Obj as short[][];
+                    if (proc == null) throw new InvalidOperationException("内部エラー: 戻り値の型が short[][] ではありません。");
+
+                    int n = Math.Min(m_frmParent.DataBlockList.Count, proc.Length);
+                    for (int i = 0; i < n; i++)
+                        m_frmParent.DataBlockList[i].SetProcessesd(proc[i], clone: true);
+                }
+                else if (ret.RetCode == ResultCode.Cancel)
+                {
+                    // ユーザーキャンセル
+                    return;
+                }
+                else // Error
+                {
+                    throw new InvalidOperationException(ret.Message);
+                }
+
+#if false
+                // 重いのでDEBUGのみ
+                try
+                {
+                    var sb = new StringBuilder();
+                    var db0 = m_frmParent.DataBlockList[0];
+                    int len = Math.Min(db0.Lf.Length, db0.Processed.Length);
+                    for (int i = 0; i < len; i++)
+                    {
+                        sb.Append(db0.Lf[i]).Append(", ").Append(db0.Processed[i]).Append('\n');
+                    }
+                    Clipboard.SetText(sb.ToString());
+                }
+                catch { /* ignore clipboard errors in debug */ }
+#endif
+
+                // 最後にUI側設定を確定
+                m_frmParent.DemodulateMode = (AnalysisForm.DemodulationMode)this.cmbDemodulate.SelectedIndex;
+                m_frmParent.ApplyBpf = bBpf;
+                m_frmParent.Hpf_kHz = hpf_kHz;
+                m_frmParent.Lpf_kHz = lpf_kHz;
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
