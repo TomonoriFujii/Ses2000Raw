e098400e865d34e6258d4a0d3ed8a6bac29225f3
Fujii Tomonori <t-fujii@windy-net.co.jp>
2025-11-10 18:34:58 +0900

    ProgressFormの追加

---
+++ Ses2000Raw/ProgressForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Ses2000Raw
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.labelDiscritption = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 50);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(325, 29);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(128, 93);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(93, 30);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // labelDiscritption
+            // 
+            this.labelDiscritption.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDiscritption.Location = new System.Drawing.Point(12, 9);
+            this.labelDiscritption.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDiscritption.Name = "labelDiscritption";
+            this.labelDiscritption.Size = new System.Drawing.Size(325, 29);
+            this.labelDiscritption.TabIndex = 5;
+            this.labelDiscritption.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 136);
+            this.ControlBox = false;
+            this.Controls.Add(this.labelDiscritption);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormProgress";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Progress";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormProgress_FormClosed);
+            this.Load += new System.EventHandler(this.frmProgress_Load);
+            this.Shown += new System.EventHandler(this.FormProgress_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label labelDiscritption;
+    }
+}
---
+++ Ses2000Raw/ProgressForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ses2000Raw
+{
+    public partial class ProgressForm : Form
+    {
+        public static bool Cancel { get; set; }
+
+        public ProgressForm()
+        {
+            InitializeComponent();
+            this.progressBar.Style = ProgressBarStyle.Blocks;
+        }
+        //public FormProgress(ProgressBarStyle style)
+        //{
+        //    InitializeComponent();
+        //    this.progressBar.Style = style;
+
+        //    if(this.progressBar.Style == ProgressBarStyle.Marquee)
+        //    {
+        //        //Cancelボタンを非表示
+        //        this.btnCancel.Visible = false;
+        //        this.Size = new System.Drawing.Size(329, 140);
+        //    }
+        //}
+        /// <summary>
+        /// Form Loadイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmProgress_Load(object sender, EventArgs e)
+        {
+            Cancel = false;
+            this.progressBar.Value = 0;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormProgress_Shown(object sender, EventArgs e)
+        {
+            this.Activate();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormProgress_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 「キャンセル」ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Cancel = true;
+        }
+
+
+        #region 公開メソッド
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prg"></param>
+        public void SetProgressValue(int value)
+        {
+            // Unit
+            if (this.progressBar.Value != value)
+            {
+                this.progressBar.Value = value;
+                this.progressBar.Update();
+            }
+            //this.label1.Text = value + "/" + this.progressBar.Maximum;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="maxValue"></param>
+        public void SetMaxValue(int maxValue)
+        {
+            this.progressBar.Maximum = maxValue;
+        }
+        /// <summary>
+        /// プログレスバーの説明文を設定します。
+        /// </summary>
+        /// <param name="unitNum"></param>
+        public void SetDiscription(string discription)
+        {
+            this.labelDiscritption.Text = discription;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="style"></param>
+        public void SetProgressStyle(ProgressBarStyle style)
+        {
+            this.progressBar.Style = style;
+            if (this.progressBar.Style == ProgressBarStyle.Marquee)
+            {
+                //Cancelボタンを非表示
+                this.btnCancel.Visible = false;
+                //this.Size = new System.Drawing.Size(329, 140);
+            }
+            else
+            {
+                this.btnCancel.Visible = true;
+            }
+        }
+        #endregion
+
+    }
+}
